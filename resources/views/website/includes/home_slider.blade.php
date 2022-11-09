d314805637321c7ab6070e9004afa59b904e5609
thekashfi <alikashfi12345@gmail.com>
2022-11-09 11:09:56 +0330

    slider

--- resources/views/website/includes/home_slider.blade.php
+++ resources/views/website/includes/home_slider.blade.php
@@ -1,25 +1,29 @@
 @if(count($practiceareaMenus))
-<section class="hero-slider hero-style-1">
-            <div class="swiper-container">
-                <div class="swiper-wrapper">
-                  @foreach($practiceareaMenus as $practiceareaMenu)
-                    <div class="swiper-slide">
-                        <div class="slide-inner slide-bg-image" data-background="@if($practiceareaMenu->bannerimage) {{url('uploads/practice/'.$practiceareaMenu->bannerimage)}} @endif" data-text="<h4>@if(app()->getLocale()=='en') {{$practiceareaMenu->menu_name_en}} @else {{$practiceareaMenu->menu_name_ar}} @endif</h4>">
-                            <div class="container">
-                                <div data-swiper-parallax="300" class="slide-title">
-                                    <h2>@if(app()->getLocale()=="en") {{$practiceareaMenu->title_en}} @else {{$practiceareaMenu->title_ar}} @endif</h2>
-                                </div>
-                            </div>
-                        </div> <!-- end slide-inner --> 
-                    </div> <!-- end swiper-slide -->
-                  @endforeach
-                </div>
-                <!-- end swiper-wrapper -->
+<section id="section-hero" class="fullwidthbanner-container" aria-label="section-slider">
+    <div id="revolution-slider">
+        <ul>
+            @foreach($practiceareaMenus as $practiceareaMenu)
+                <li data-transition="fade" data-slotamount="10" data-masterspeed="200" data-thumb="">
+                    <!--  BACKGROUND IMAGE -->
+                    <img src="@if($practiceareaMenu->bannerimage) {{url('uploads/practice/'.$practiceareaMenu->bannerimage)}} @endif" alt="" />
+                    <div class="tp-caption big-white sft" data-x="0" data-y="150" data-speed="800" data-start="400" data-easing="easeInOutExpo"
+                         data-endspeed="450">
+                        @if(app()->getLocale()=="en") {{$practiceareaMenu->title_en}} @else {{$practiceareaMenu->title_ar}} @endif
+                    </div>
 
-                <!-- swipper controls -->
-                <div class="swiper-cust-pagination"></div>
-                <div class="swiper-button-next"></div>
-                <div class="swiper-button-prev"></div>
-            </div>
-        </section>
-  @endif      
+                    <div class="tp-caption ultra-big-white customin customout start" data-x="0" data-y="center" data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:2;scaleY:2;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                         data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.85;scaleY:0.85;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
+                         data-speed="800" data-start="400" data-easing="easeInOutExpo" data-endspeed="400">
+                        @if(app()->getLocale()=='en') {{$practiceareaMenu->menu_name_en}} @else {{$practiceareaMenu->menu_name_ar}} @endif
+                    </div>
+
+                    <div class="tp-caption sfb" data-x="0" data-y="335" data-speed="400" data-start="800" data-easing="easeInOutExpo">
+                        <a href="{{url('/practice/'.$practiceareaMenu->slug)}}" class="btn-slider">More Details
+                        </a>
+                    </div>
+                </li>
+            @endforeach
+        </ul>
+    </div>
+</section>
+@endif
--- resources/views/website/index.blade.php
+++ resources/views/website/index.blade.php
@@ -85,72 +85,7 @@
             </section> -->
 
             <!-- revolution slider begin -->
-            <section id="section-hero" class="fullwidthbanner-container" aria-label="section-slider">
-                <div id="revolution-slider">
-                    <ul>
-                        <li data-transition="fade" data-slotamount="10" data-masterspeed="200" data-thumb="">
-                            <!--  BACKGROUND IMAGE -->
-                            <img src="{{url('new_assets')}}/images/slider/wide1.jpg" alt="" />
-                            <div class="tp-caption big-white sft" data-x="0" data-y="150" data-speed="800" data-start="400" data-easing="easeInOutExpo"
-                                 data-endspeed="450">
-                                Our Expertise For
-                            </div>
-
-                            <div class="tp-caption ultra-big-white customin customout start" data-x="0" data-y="center" data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:2;scaleY:2;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-                                 data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.85;scaleY:0.85;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-                                 data-speed="800" data-start="400" data-easing="easeInOutExpo" data-endspeed="400">
-                                Interior Design
-                            </div>
-
-                            <div class="tp-caption sfb" data-x="0" data-y="335" data-speed="400" data-start="800" data-easing="easeInOutExpo">
-                                <a href="#" class="btn-slider">Our Portfolio
-                                </a>
-                            </div>
-                        </li>
-
-                        <li data-transition="fade" data-slotamount="10" data-masterspeed="200" data-thumb="">
-                            <!--  BACKGROUND IMAGE -->
-                            <img src="{{url('new_assets')}}/images/slider/wide2.jpg" alt="" />
-                            <div class="tp-caption big-white sft" data-x="0" data-y="160" data-speed="800" data-start="400" data-easing="easeInOutExpo"
-                                 data-endspeed="450">
-                                Featured Project
-                            </div>
-
-                            <div class="tp-caption ultra-big-white customin customout start" data-x="0" data-y="center" data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:2;scaleY:2;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-                                 data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.85;scaleY:0.85;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-                                 data-speed="800" data-start="400" data-easing="easeInOutExpo" data-endspeed="400">
-                                Green Interior
-                            </div>
-
-                            <div class="tp-caption sfb" data-x="0" data-y="335" data-speed="400" data-start="800" data-easing="easeInOutExpo">
-                                <a href="#" class="btn-slider">Our Portfolio
-                                </a>
-                            </div>
-                        </li>
-
-                        <li data-transition="fade" data-slotamount="10" data-masterspeed="200" data-thumb="">
-                            <!--  BACKGROUND IMAGE -->
-                            <img src="{{url('new_assets')}}/images/slider/wide3.jpg" alt="" />
-                            <div class="tp-caption big-white sft" data-x="0" data-y="160" data-speed="800" data-start="400" data-easing="easeInOutExpo"
-                                 data-endspeed="450">
-                                Interior Remodeling To Makes
-                            </div>
-
-                            <div class="tp-caption ultra-big-white customin customout start" data-x="0" data-y="center" data-customin="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:2;scaleY:2;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-                                 data-customout="x:0;y:0;z:0;rotationX:0;rotationY:0;rotationZ:0;scaleX:0.85;scaleY:0.85;skewX:0;skewY:0;opacity:0;transformPerspective:600;transformOrigin:50% 50%;"
-                                 data-speed="800" data-start="400" data-easing="easeInOutExpo" data-endspeed="400">
-                                Your Life Easier
-                            </div>
-
-                            <div class="tp-caption sfb" data-x="0" data-y="335" data-speed="400" data-start="800" data-easing="easeInOutExpo">
-                                <a href="#" class="btn-slider">Our Portfolio
-                                </a>
-                            </div>
-                        </li>
-
-                    </ul>
-                </div>
-            </section>
+            @include('website.includes.home_slider')
             <!-- revolution slider close -->
 
             <!-- section begin -->
